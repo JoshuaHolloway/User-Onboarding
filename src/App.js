c39d339f3d3b21d0ab7c43e927e51fdef70e86d9
JoshuaHolloway <jhollow6@asu.edu>
2020-09-15 14:47:57 -0500

    adding new user with form data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,66 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 // Components4
 import Form from './Form';
 
+// Initial values to be placed in program state at startup
+const usersList = [
+  { name: 'josh'},
+  { name: 'mike'}
+];
+
+const initialFormValues = {
+  name: ''
+};
+
 function App() {
+  const [users, setUsers] = useState(usersList);
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  const change = (event) => {
+    const { name, value } = event.target;
+
+    console.log('input change, name: ', name, ', value: ', value, ', formValues: ', formValues);
+
+    setFormValues({ ...formValues, [name]: value});
+  };
+
+  const submit = (event) => {
+    event.preventDefault(); // don't navigate
+
+    const newUser = { // create new user from form data
+      name: formValues.name
+    };
+
+    setUsers([...users, newUser]);    // Append new user
+    setFormValues(initialFormValues); // Reset form
+  };
+
   return (
     <div className="App">
-      <Form />
+      {
+        users.map((user, idx) => {
+          return <div key={idx}>UserName: {user.name}</div>;
+        })
+      }
+      
+      <form onSubmit={submit}>
+        <h5>Enter Your Info: </h5>
+        <label htmlFor="name">Name: 
+          <input 
+            id="name"
+            name="name"
+            type="text"
+            value={formValues.name}
+            onChange={change}
+          />
+        </label>
+
+        <button>Create Accoutn</button>
+      </form>
+      
     </div>
   );
 }
--- src/Form.js
+++ src/Form.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Form = () => {
+
+    const [name, setName] = useState('');
+
+    const buttonClick = (event) => {
+        console.log('clicked');
+
+        // Update state here
+        //setName();
+    };
+
     return (
-        <h1>Form.js</h1>
+        <form>
+            <label htmlFor="name">Name: 
+                <input id="name" name="name" type="text"/>
+            </label>
+            
+            <button onClick={buttonClick}>Submit</button>
+        </form>
     );
 };
 export default Form;
